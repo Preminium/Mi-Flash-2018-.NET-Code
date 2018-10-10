262dacf7db3872ccad550c70cdfcde1cb3b74cdf
Preminium <44021833+Preminium@users.noreply.github.com>
2018-10-10 03:34:59 -0700

    Delete MiBaseFrm.Designer.cs

--- MiBaseFrm.Designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XiaoMiFlash
-{
-	// Token: 0x02000011 RID: 17
-	public partial class MiBaseFrm : global::System.Windows.Forms.Form, global::XiaoMiFlash.code.lan.ILanguageSupport
-	{
-	}
-}
