e3ddbadd1a94159b3ca42fe2e731430225bd4565
Preminium <44021833+Preminium@users.noreply.github.com>
2018-10-10 03:40:43 -0700

    Add files via upload

---
+++ MiBaseFrm.Designer.cs
@@ -0,0 +1,7 @@
+﻿namespace XiaoMiFlash
+{
+	// Token: 0x02000011 RID: 17
+	public partial class MiBaseFrm : global::System.Windows.Forms.Form, global::XiaoMiFlash.code.lan.ILanguageSupport
+	{
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XiaoMiFlash
+{
+	// Token: 0x0200000F RID: 15
+	internal static class Program
+	{
+		// Token: 0x0600007A RID: 122 RVA: 0x00009207 File Offset: 0x00007407
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainFrm());
+		}
+	}
+}
